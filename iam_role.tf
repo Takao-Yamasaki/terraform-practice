429262610f4b38e24fd74bcd1b3e6f082df18c22
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-09-16 20:00:34 +0900

    add:IAMロールの定義

--- iam_role.tf
+++ iam_role.tf
@@ -13,3 +13,28 @@ resource "aws_iam_policy" "example" {
   name = "example"
   policy = data.aws_iam_policy_document.allow_describe_regions.json
 }
+
+# 信頼ポリシー
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      # EC2のみに関連付けできる
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+# IAMロールの定義
+resource "aws_iam_role" "example" {
+  name = "example"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
+# IAMポリシーのアタッチ
+resource "aws_iam_role_policy_attachment" "example" {
+  role = aws_iam_role.example.name
+  policy_arn = aws_iam_policy.example.arn
+}
