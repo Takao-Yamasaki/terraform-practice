99723a3ad7a57b193267071a9f68df1ed897b070
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-09-17 14:38:15 +0900

    add:インターネットゲートウェイの設定

--- network.tf
+++ network.tf
@@ -11,3 +11,21 @@ resource "aws_vpc" "example" {
     Name = "example"
   }
 }
+
+# パブリックサブネットの定義
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "ap-northeast-1a"
+}
+
+# インターネットゲートウェイの定義
+resource "aws_internet_gateway" "example" {
+  vpc_id = aws_vpc.example.id
+}
+
+# ルートテーブルの定義
+resource "aws_route_table" "example" {
+  vpc_id = aws_vpc.example.id
+}
