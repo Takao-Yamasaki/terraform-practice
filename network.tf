7f454b155eaf2b654e47773d8ee0c749f318b331
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-09-17 15:18:02 +0900

    add:ルートの定義

--- network.tf
+++ network.tf
@@ -26,6 +26,19 @@ resource "aws_internet_gateway" "example" {
 }
 
 # ルートテーブルの定義
-resource "aws_route_table" "example" {
+resource "aws_route_table" "public" {
   vpc_id = aws_vpc.example.id
+}
+
+# ルートの定義
+resource "aws_route" "public" {
+  route_table_id = aws_route_table.public.id
+  gateway_id = aws_internet_gateway.example.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+# ルートテーブルの関連付け
+resource "aws_route_table_association" "public" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
