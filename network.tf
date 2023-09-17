27b4eea44c16af55e431c80bc945cdc5be22a639
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-09-17 15:55:36 +0900

    add:NATゲートウェイ

--- network.tf
+++ network.tf
@@ -56,10 +56,26 @@ resource "aws_subnet" "private" {
 # NOTE: プライベートなので、インターネットゲートウェイに対するルーティングは不要
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.example.id
+# NOTE: NATゲートウェイに対するルーティングを追加
+  nat_gateway_id = aws_nat_gateway.example.id
+  destination_cidr_block = "0.0.0.0/0"
 }
 
 # ルートテーブルの関連付け
 resource "aws_route_table_association" "private" {
   subnet_id = aws_subnet.private.id
   route_table_id = aws_route_table.private.id
+}
+
+# EIPの定義
+resource "aws_eip" "nat_gateway" {
+  vpc = true
+  depends_on = [ aws_internet_gateway.example ]
+}
+
+# NATゲートウェイの定義
+resource "aws_nat_gateway" "example" {
+  allocation_id = aws_eip.nat_gateway.id
+  subnet_id = aws_subnet.public.id
+  depends_on = [ aws_internet_gateway.example ]
 }
